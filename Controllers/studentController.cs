f22069fe5368ae0b876751c6f80cb7fec7f525be
mahinfah <mahinfah1234@gmail.com>
2026-06-28 00:22:24 +0600

    ok

---
+++ Controllers/studentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace labtask.net.Controllers
+{
+    public class studentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
