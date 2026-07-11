e16b0b73bdd3c8e8f2841f1a767397b9c7d8bf1e
Kazi Fahad Mahin <151399467+mahinfah@users.noreply.github.com>
2026-07-11 21:41:32 +0600

    Format ErrorViewModel.cs for improved readability

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,7 +1,9 @@
 namespace labtask.net.Models
 {
     public class ErrorViewModel
-    {
+    
+
+        
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
